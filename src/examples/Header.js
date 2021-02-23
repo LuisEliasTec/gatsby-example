c8a8869415d9b065740d3fb8595c13bb0166dff4
LuisEliasTec <luiselias_1@hotmail.com>
2021-02-23 12:18:47 -0700

    initial commit

---
+++ src/examples/Header.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+const getData = graphql`
+  {
+    site {
+      info: siteMetadata {
+        author
+        data
+        description
+        title
+        person {
+          age
+          name
+        }
+      }
+    }
+  }
+`
+
+const Header = () => {
+  // const data = useStaticQuery(getData)
+
+  const {
+    site: {
+      info: {
+        title,
+        person: { name },
+      },
+    },
+  } = useStaticQuery(getData)
+
+  return (
+    <div>
+      <h1>Title: {title}</h1>
+      <h1>Name: {name}</h1>
+    </div>
+  )
+}
+
+export default Header
